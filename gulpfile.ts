bbaf3c5c31b98a2ef1b2edec979c15ddeca92a51
Adam <adam.genshaft@globality-contractor.com>
2019-10-27 21:05:52 +0200

    working stable but slow

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,26 +1,46 @@
-import { src, dest, watch } from 'gulp';
+import gulp, { src, dest, watch } from 'gulp';
 import livereload from 'gulp-livereload';
 import typescript from 'gulp-typescript';
 import sourcemaps from 'gulp-sourcemaps';
+import through2 from 'through2';
 
 const tsProject = typescript.createProject('tsconfig.json');
+const tsSrc = tsProject.src();
+const prependShebang = function generatePrependShebang() {
+    return through2.obj(function prependShebang(file, _, cb) {
+        if (file.isBuffer()) {
+            file.contents = Buffer.from(`#!/usr/bin/env node\n${file.contents.toString()}`);
+        }
 
-async function ts() {
-    return src('*.ts')
-        .pipe(sourcemaps.init())
-        .pipe(tsProject())
-        .pipe(sourcemaps.write())
-        .pipe(dest('dist'))
-        .pipe(livereload());
+        cb(null, file);
+    });
 }
 
-async function watchTs() {
+export async function ts() {
+    await tsSrc.pipe(sourcemaps.init())
+        .pipe(tsProject())
+        .pipe(sourcemaps.write())
+        .pipe(prependShebang())
+        .pipe(dest(tsProject.options.outDir))
+    livereload.reload();
+    return;
+}
+
+export async function watchTs() {
     livereload.listen();
-    return watch('*.ts', async function watchFSChangeTsCallback(cb) {
+    console.log('starting initial ts');
+    await ts();
+    console.log('finished initial ts, watching file changes');
+    return watch('**/*.ts', async function watchFSChangeTsCallback(cb) {
         await ts();
         cb();
     });
 }
 
-
 export default watchTs;
+
+const isEntryScript = require.main === module;
+
+if (isEntryScript) {
+    watchTs();
+}
